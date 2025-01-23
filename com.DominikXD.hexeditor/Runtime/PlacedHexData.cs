232ad5163b7ff1eeae01779a6239b835fe610e11
Kolouch-Dominik <114079305+Kolouch-Dominik@users.noreply.github.com>
2025-01-23 21:43:16 +0100

    Add files via upload

---
+++ com.DominikXD.hexeditor/Runtime/PlacedHexData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+[Serializable]
+public class PlacedHexData
+{
+    public int q;
+    public int r;
+    public string prefabGuid;
+    public float rotationY;
+    public float posX;
+    public float posY;
+    public float posZ;
+}
---
+++ com.DominikXD.hexeditor/Runtime/PlacedHexInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+using UnityEngine;
+
+[Serializable]
+public class PlacedHexInfo
+{
+    public string prefabGUID;
+    public GameObject instance;
+    public float rotationY;
+}
